b361cc8ff57902ff456b0daafdc16a224fd53a0d
RiccardoBeca <riccardobeca@gmail.com>
2022-06-23 15:37:24 +0100

    header/footer added

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,98 @@
+
+
+  <footer>
+    <div class="footer-top">
+
+      <div class="w-70">
+        <ul>
+          <li>
+            <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+            <p>DIGITAL COMICS</p>
+          </li>
+           <li>
+            <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+            <p>DC MERCHANDISE</p>
+          </li>
+           <li>
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+            <p>SUBSCRIPTION</p>
+          </li>
+           <li>
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+            <p>COMIC SHOP LOCATION</p>
+          </li>
+           <li>
+            <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+            <p>DC POWER VISA</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-mid">
+      <div class="w-70">
+        <div class="list-container">
+             <div class="left-col">
+          <ul>
+            <h2>DC COMICS</h2>
+            <li><a href="">Characters</a></li>
+            <li><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Tv</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">News</a></li>
+          </ul>
+            <ul>
+            <h2>SHOP</h2>
+            <li><a href="">Shop DC</a></li>
+            <li><a href="">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+        <div class="mid-col">
+          <ul>
+            <h2>DC</h2>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Privacy Policy(New)</a></li>
+            <li><a href="">Ad Choices</a></li>
+            <li><a href="">Advertising</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Subscription</a></li>
+            <li><a href="">Talent Workshop</a></li>
+            <li><a href="">CPSC Certificates</a></li>
+            <li><a href="">Ratings</a></li>
+            <li><a href="">Shop Help</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="right-col">
+          <ul>
+            <h2>SITES</h2>
+            <li><a href="">DC</a></li>
+            <li><a href="">MAD Magazine</a></li>
+            <li><a href="">DC Kids</a></li>
+            <li><a href="">DC Universe</a></li>
+            <li><a href="">DC Power Visa</a></li>
+          </ul>
+        </div>
+        </div>
+     
+        <div class="bg-logo">
+          <img src="{{asset('img/dc-logo-bg.png')}}" alt="Footer-bg">
+        </div>
+      </div>
+    </div>
+    <div class="footer-btm">
+      <div class="container w-70">
+        <button><a href="#">SIGN-UP NOW!!</a></button>
+        <ul>
+          <h2>FOLLOW US</h2>
+          <li><img src="../assets/img/footer-facebook.png" alt=""></li>
+          <li><img src="../assets/img/footer-periscope.png" alt=""></li>
+          <li><img src="../assets/img/footer-pinterest.png" alt=""></li>
+          <li><img src="../assets/img/footer-twitter.png" alt=""></li>
+          <li><img src="../assets/img/footer-youtube.png" alt=""></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+  <div class="container w-70">
+    <div class="logo">
+     <img src="{{asset('img/dc-logo.png')}}" alt="DC Logo">
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a href="">CHARACTERS</a>
+        </li>
+        <li>
+          <a class="{{ Route::currentRouteName() ==='home' ? 'active' : '' }}" href="{{ route('home') }}">COMICS</a>
+
+        </li>
+        <li>
+          <a href="">MOVIES</a>
+        </li>
+        <li>
+          <a href="">TV</a>
+        </li>
+        <li>
+          <a href="">GAMES</a>
+        </li>
+        <li>
+          <a href="">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="">VIDEOS</a>
+        </li>
+        <li>
+          <a href="">FANS</a>
+        </li>
+        <li>
+          <a href="">NEWS</a>
+        </li>
+        <li>
+          <a href="">SHOP</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>

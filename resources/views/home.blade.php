0ff1e347e776823610aae9aa858f456cfd838df0
RiccardoBeca <riccardobeca@gmail.com>
2022-06-23 15:47:39 +0100

    jumbo background fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-
+<main>
+  <div class="jumbotron"></div>
+   <div class="prova">
+      <div class="w-70 cards-container">
+        <CardComp
+         v-for="(book, index) in booksList" :key="`book${index}`"
+         :thumb="book.thumb"
+         :series="book.series"
+        />  
+        <button><a href="#">LOAD MORE</a></button>
+      </div>
+  </div>
+  
+</main>
 
 @endsection
